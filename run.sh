d121be36171bb9f79d6510e903fd906458284d93
Billy <billybofh@gmail.com>
2019-12-16 13:49:45 +0000

    Updated playbook to work with ubuntu or debian

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 docker run -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
   -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
   -v $(pwd):/ansible/playbooks \
-  uogsoe/ansible-playbook docker_ubuntu.yml -i /ansible/playbooks/hosts
+  uogsoe/ansible-playbook install_docker.yml -i /ansible/playbooks/hosts
 
