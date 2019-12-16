f457b29a7d881f90a0055be75565436019579b2a
Billy <billybofh@gmail.com>
2019-12-16 13:51:16 +0000

    Added a note to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# assuming you've done something like :
+#   docker build -t uogsoe/ansible-playbook .
+
 docker run -v ~/.ssh/id_rsa:/root/.ssh/id_rsa \
   -v ~/.ssh/id_rsa.pub:/root/.ssh/id_rsa.pub \
   -v $(pwd):/ansible/playbooks \
